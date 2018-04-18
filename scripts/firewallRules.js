5039acffc1b1654b7705dae9b318c465f485d50e
dzotic9 <dzotic9@gmail.com>
2018-04-18 09:23:48 +0300

    Update firewallRules.js

--- scripts/firewallRules.js
+++ scripts/firewallRules.js
@@ -1,39 +1,41 @@
-var version = jelastic.system.service.GetVersion().version.split("-").shift(),
+var PARAM_UNINSTALL = 'uninstall',
+    PARAM_INSTALL = 'install',
+    DIRECTION_OUT = 'OUTPUT',
+    DIRECTION_IN = 'INPUT',
+    RSYNC_PORT = '7755',
+    ALLOW = 'ALLOW',
+    SSH = 'SSH',
+    ALL = 'ALL',
+    CP = 'cp',
     envName = "${env.name}",
     outputRule,
     inputRule,
     rules,
     resp;
 
-inputRule = {"direction":"INPUT","name":"SSH","protocol":"ALL","ports":"7755","src":"ALL","priority":1080,"action":"ALLOW"};
-outputRule = {"direction":"OUTPUT","name":"SSH","protocol":"ALL","ports":"7755","dst":"ALL","priority":1000,"action":"ALLOW"};
+inputRule = {"direction":DIRECTION_IN,"name":SSH,"protocol":ALL,"ports":RSYNC_PORT,"src":ALL,"priority":1080,"action":ALLOW};
+outputRule = {"direction":DIRECTION_OUT,"name":SSH,"protocol":ALL,"ports":RSYNC_PORT,"dst":ALL,"priority":1000,"action":ALLOW};
 
-if (compareVersions(version, '5.3')){
-  if (param == 'install') {
+if (jelastic.environment.security.AddRule){
+  if (param == PARAM_INSTALL) {
       
-    resp = jelastic.environment.security.AddRule(envName, session, inputRule, 'cp');
+    resp = jelastic.environment.security.AddRule(envName, session, inputRule, CP);
     if (!resp || resp.result !== 0) return resp;
-    return jelastic.environment.security.AddRule(envName, session, outputRule, 'cp');
+    return jelastic.environment.security.AddRule(envName, session, outputRule, CP);
 
-  } else if (param == 'uninstall') {
-    resp = removeRule('IN');
+  } else if (param == PARAM_UNINSTALL) {
+    resp = removeRule(DIRECTION_IN);
     if (!resp || resp.result !== 0) return resp;
-    return removeRule('OUT');
+    return removeRule(DIRECTION_OUT);
   }
 }
 
 function removeRule(InOutBond) {
-    var rules = jelastic.environment.security.GetRules('${env.name}', session, 'cp', InOutBond).rules;
+    var rules = jelastic.environment.security.GetRules('${env.name}', session, CP, InOutBond).rules;
 
     for (var i = 0; i < rules.length; i ++) {
         if (rules[i].ports == 7755) {
             return jelastic.environment.security.RemoveRule(envName, session, rules[i].id);
         }
     }
-}
-
-function compareVersions(a, b) {
-  a = a.split("."), b = b.split(".")
-  for (var i = 0, l = Math.max(a.length, b.length); i < l; i++) {x = parseInt(a[i], 10) || 0; y = parseInt(b[i], 10) || 0; if (x != y) return x > y ? 1 : -1 }
-  return 0;
 }
