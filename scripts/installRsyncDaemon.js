4bd76c868d9452ce7756edfe55552236cf3937bc
dzotic91-ukr <45935484+dzotic91-ukr@users.noreply.github.com>
2019-07-10 16:00:28 +0300

    JE-44549

--- scripts/installRsyncDaemon.js
+++ scripts/installRsyncDaemon.js
@@ -2,25 +2,26 @@
 // @secondEnvAddress
 // @syncPassword
 
-var NODE_GROUP_COMPUTE = "cp",
+var NODE_GROUP_STORAGE = "storage",
     PROCEDURE_PROCESS_NODE = "installLsync",
     APPID = getParam("TARGET_APPID"),
     SESSION = getParam("session"),
+    USER = "jelastic",
     callArgs = [],
-    env,
-    nodes,
-    i, j,
-    n,
-    softNode,
-    sServerWebroot,
-    lsyncdPath,
-    user = "",
-    computeNodes = [],
-    aCpItemEnvs = [],
-    envInfoResponse,
     syncPassword = "${settings.password}",
     secondAddress = "${settings.address}",
-    settingsPath = "${settings.path}";
+    settingsPath = "${settings.path}",
+    isStorageNode = false,
+    envLsyncdNodes = [],
+    aCpItemEnvs = [],
+    envInfoResponse,
+    callArgs = [],
+    lsyncdPath,
+    nodes,
+    env,
+    i,
+    j,
+    n;
 
 if (syncPassword.indexOf("{settings.password}") != -1) {
     syncPassword = "defaultPassword";
@@ -43,29 +44,29 @@ if (envInfoResponse.result != 0) {
 nodes = envInfoResponse.nodes;
 
 for (i = 0, n = nodes.length; i < n; i += 1) {
-
-    if (NODE_GROUP_COMPUTE == nodes[i].nodeGroup) {
-        computeNodes.push(nodes[i]);
+    if ("${targetNodes.nodeGroup}" == nodes[i].nodeGroup) {
+        isStorageNode = !!(NODE_GROUP_STORAGE == nodes[i].nodeType);
+        envLsyncdNodes.push(nodes[i]);
     }
 }
 
-if (computeNodes[0].type == "DOCKERIZED") {
-	
-    if (!computeNodes[0].engines) {
-	aCpItemEnvs = computeNodes[0].customitem.dockerManifest.env;
-	    
-	    for (j = 0; aCpItemEnvs[j]; j += 1) {
-		    
-		if (aCpItemEnvs[j].indexOf('WEBROOT') != -1) {
-		    sServerWebroot = aCpItemEnvs[j].replace('WEBROOT=', '');
-			lsyncdPath = sServerWebroot + '/';
-			break;
-		}
-	    }
+if (envLsyncdNodes[0].type == "DOCKERIZED") {
+    if (!envLsyncdNodes[0].engines) {
+        aCpItemEnvs = envLsyncdNodes[0].customitem.dockerManifest.env;
+
+        for (j = 0; aCpItemEnvs[j]; j += 1) {
+
+            if (aCpItemEnvs[j].indexOf('WEBROOT') != -1) {
+                lsyncdPath = aCpItemEnvs[j].replace('WEBROOT=', '') + '/';
+                break;
+            }
+        }
     }
 }
 
-if (!lsyncdPath) lsyncdPath = "${SERVER_WEBROOT}/";
+if (!lsyncdPath) {
+    lsyncdPath = isStorageNode ? "/data/" : "${SERVER_WEBROOT}/"
+}
 			
 if (settingsPath != "/" || settingsPath != "") {
     settingsPath = settingsPath.replace(lsyncdPath, '');
@@ -73,22 +74,21 @@ if (settingsPath != "/" || settingsPath != "") {
 	
 user = "jelastic";
 
-for (var i = 0, n = computeNodes.length; i < n; i += 1) {
-    var mirrorServerIp = computeNodes[(i + 1) === computeNodes.length ? 0 : i + 1].address;
+for (var i = 0, n = envLsyncdNodes.length; i < n; i += 1) {
+    var mirrorServerIp = envLsyncdNodes[(i + 1) === envLsyncdNodes.length ? 0 : i + 1].address;
 
     callArgs.push({
         procedure : PROCEDURE_PROCESS_NODE,
         params : {
-            nodeId : computeNodes[i].id,
+            nodeId : envLsyncdNodes[i].id,
             mirrorServerIp : mirrorServerIp,
             settingsPath : settingsPath,
             secondEnvAddress : secondAddress,
             syncPassword : syncPassword,
             lsyncdPath : lsyncdPath,
-            user : user
+            user : USER
         }
     });
-
 } 
 
 return {
