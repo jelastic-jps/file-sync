43a779207d82e1277f1b8d59490c6d4336944a8c
dzotic91-ukr <45935484+dzotic91-ukr@users.noreply.github.com>
2019-07-23 14:55:20 +0300

    JE-48777

--- scripts/twoEnvs.sh
+++ scripts/twoEnvs.sh
@@ -8,7 +8,7 @@ lsyncdPath=$3
 grep -q "@"${EXT_IP}"/" ${lsyncdPath}/lsyncd/etc/lsyncd.conf || echo "sync {
 		default.rsync,
 		source=\"${lsyncdPath}${settingsPath}\",
-                target=\"rsync://admin@"${EXT_IP}"/secondenvmodule\",
+                target=\"rsync://admin@"${EXT_IP}"/syncmodule\",
 		delay=10,
                 delete='running',
 		exclude = {
